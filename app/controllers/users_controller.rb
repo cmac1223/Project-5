404c4ace2a0eeae2951dd17bbc5350436fce36a8
cmac1223 <chrismac539@gmail.com>
2017-07-10 12:32:51 -0400

    update user routes is complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
-  end
+    def index
+      @users = User.all
+      render json: @users
+    end
 
-  def show
-    @user = User.find(params[:id])
-    render json: {
-                  user: @user
-                  }
-  end
+    def show
+      @user = User.find(params[:id])
+      render json: {
+                    user: @user
+                    }
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        render json:{
+          user: @user
+        }
+        end
+    end
+
+    private
+    def user_params
+      params.require(:user).permit(:First_Name, :Last_Name, :Name, :About)
+    end
+    
 end
+
