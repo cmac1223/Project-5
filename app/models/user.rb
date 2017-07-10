81abdc20c494d7ccfbfc96addda41f31778fb2a2
cmac1223 <chrismac539@gmail.com>
2017-07-10 10:18:31 -0400

    has_one association between user and gallery

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :uevents
   has_many :events, through: :uevents
+  has_one :gallery
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
