8e7e06dfd22ddd3ebd232444a6f85a7cf926da7a
cmac1223 <chrismac539@gmail.com>
2017-07-17 13:19:47 -0400

    added api geo coding key to address

--- client/components/map/map.event/map.controller.js
+++ client/components/map/map.event/map.controller.js
@@ -3,7 +3,7 @@ MapController.$inject = ["NgMap", "$http"]
 function MapController(NgMap, $http){
   const vm = this;
   vm.marker = {};
-  vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+  vm.googleMapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json&key=AIzaSyBDVtORwEP8IA1W2GBzUQgjudgFBqpgcjs';
 
   // activate();
   vm.activate = activate;
